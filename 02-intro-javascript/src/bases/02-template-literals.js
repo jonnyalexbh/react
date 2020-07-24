28d22035c4686733ce189af00a5385a1da310011
jonnyalexbh <jonnyalex.bh@gmail.com>
2020-07-24 17:26:25 -0500

    template literals

---
+++ 02-intro-javascript/src/bases/02-template-literals.js
@@ -0,0 +1,12 @@
+const name = 'Jonny';
+const lastName = 'Hurtado';
+
+const fullName = `His full name is  ${name} ${lastName}`;
+
+console.log(fullName);
+
+function getGreeting() {
+  return 'Hello World';
+}
+
+console.log(`this is a greeting ${getGreeting()}`);
