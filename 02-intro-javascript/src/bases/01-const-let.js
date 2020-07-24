c87f6d4e3ddb8278cafcc35c36246e382ebee18d
jonnyalexbh <jonnyalex.bh@gmail.com>
2020-07-24 17:10:51 -0500

    variables and constants

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,16 @@
+// variables and constants
+
+const name = 'Jonny';
+const last_name = 'Hurtado';
+
+let valueDice = 5;
+valueDice = 4;
+
+console.log(name, last_name, valueDice);
+
+if (true) {
+  const name = 'Tankis';
+  console.log(name);
+}
+
+console.log(name);
