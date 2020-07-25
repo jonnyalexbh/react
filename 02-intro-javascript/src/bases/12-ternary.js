fb1bf7f9eede47dfa02061ffc53fb8e328268b16
jonnyalexbh <jonnyalex.bh@gmail.com>
2020-07-25 14:40:50 -0500

    ternary

---
+++ 02-intro-javascript/src/bases/12-ternary.js
@@ -0,0 +1,17 @@
+const actived = true;
+
+let message = '';
+
+// if ( !actived ) {
+//     message = 'actived';
+// } else {
+//     message = 'Inactived';
+// }
+
+const message = (actived) ? 'actived' : 'Inactived';
+// const message = ( actived ) ? 'actived' : null; 
+
+// const message = actived && 'actived';
+
+
+console.log(message);
